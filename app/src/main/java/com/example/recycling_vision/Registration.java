5f55a11c11143b2902ad64babd74fd73701e039a
Anojan Sothilingam <anojansothilingam35@gmail.com>
2020-06-29 21:28:13 -0400

    updated Registration class

--- app/src/main/java/com/example/recycling_vision/Registration.java
+++ app/src/main/java/com/example/recycling_vision/Registration.java
@@ -10,5 +10,8 @@ public class Registration extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
     }
 }
