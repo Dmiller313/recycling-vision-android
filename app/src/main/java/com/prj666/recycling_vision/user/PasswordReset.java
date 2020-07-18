d11f5b71257f9eac4efd55144d0d153923802d00
Anojan Sothilingam <anojansothilingam35@gmail.com>
2020-07-17 20:29:42 -0400

    added password reset

---
+++ app/src/main/java/com/prj666/recycling_vision/user/PasswordReset.java
@@ -0,0 +1,16 @@
+package com.prj666.recycling_vision.user;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.prj666.recycling_vision.R;
+
+public class PasswordReset extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_password_reset);
+    }
+}
