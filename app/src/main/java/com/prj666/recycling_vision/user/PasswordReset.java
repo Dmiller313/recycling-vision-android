db48d93f0af420d26c109db70245fe32054f3431
David Miller <dmiller23@myseneca.ca>
2020-08-03 12:07:59 -0400

    password reset fixed

--- app/src/main/java/com/prj666/recycling_vision/user/PasswordReset.java
+++ app/src/main/java/com/prj666/recycling_vision/user/PasswordReset.java
@@ -26,10 +26,8 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class PasswordReset extends AppCompatActivity {
-
+    private static final String PASSWORD_PATTERN = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!*@#$%^&+=])(?=\\S+$).{8,}$";
     private TextView email, password, newPassword;
-    private Button reset, cancel;
-    private String res, res1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,88 +36,54 @@ public class PasswordReset extends AppCompatActivity {
         email = findViewById(R.id.edtTxtEmail);
         password = findViewById(R.id.edtTxtPassword);
         newPassword = findViewById(R.id.edtTxtNewPassword);
-        reset = findViewById(R.id.btnReset);
-        cancel = findViewById(R.id.cancel);
+        Button reset = findViewById(R.id.btnReset);
+        Button cancel = findViewById(R.id.cancel);
 
 
 
         reset.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                RequestQueue queue = Volley.newRequestQueue(getBaseContext());
+                if(validateData()) {
+                    RequestQueue queue = Volley.newRequestQueue(getBaseContext());
 
-                String url = "https://recycling-vision.herokuapp.com/exists";
-                Map<String, String> jsonData = new HashMap<>();
+                    String url = "https://recycling-vision.herokuapp.com/passwordreset";
+                    Map<String, String> jsonData = new HashMap<>();
+                    jsonData.put("email", email.getText().toString());
+                    jsonData.put("password", password.getText().toString());
+                    jsonData.put("newPassword", newPassword.getText().toString());
 
-                jsonData.put("email", email.getText().toString());
+                    JSONObject json = new JSONObject(jsonData);
 
-                JSONObject json = new JSONObject(jsonData);
+                    JsonObjectRequest request = new JsonObjectRequest(
+                            Request.Method.POST, url, json, new Response.Listener<JSONObject>() {
 
-                JsonObjectRequest request = new JsonObjectRequest(
-                        Request.Method.GET, url, json, new Response.Listener<JSONObject>() {
-
-                    @Override
-                    public void onResponse(JSONObject response) {
-                        try {
-                            res = response.getString("status");
-                            if(res.equals("exists")){
-                                RequestQueue queue1 = Volley.newRequestQueue(getBaseContext());
-
-                                String url1 = "https://recycling-vision.herokuapp.com/passwordreset";
-                                Map<String, String> jsonData1 = new HashMap<>();
-                                jsonData1.put("email", email.getText().toString());
-                                jsonData1.put("password", password.getText().toString());
-                                jsonData1.put("newPassword", newPassword.getText().toString());
-
-                                JSONObject json1 = new JSONObject(jsonData1);
-
-                                JsonObjectRequest request1 = new JsonObjectRequest(
-                                        Request.Method.POST, url1, json1, new Response.Listener<JSONObject>() {
-
-                                    @Override
-                                    public void onResponse(JSONObject response) {
-                                        try {
-                                            res1 = response.getString("status");
-                                            if(res1.equals("success")){
-                                                Intent login = new Intent(PasswordReset.this, Login.class);
-                                                PasswordReset.this.startActivity(login);
-                                            }else if(res1.equals("error")){
-                                                Toast.makeText(getApplicationContext(), "An Error has occurred try again", Toast.LENGTH_LONG).show();
-                                            } else if(res1.equals("unauthorized")){
-                                                Toast.makeText(getApplicationContext(), "You entered and Invalid password", Toast.LENGTH_LONG).show();
-                                            }else {
-                                                Toast.makeText(getApplicationContext(), "An Error has occurred try again", Toast.LENGTH_LONG).show();
-                                            }
-                                        } catch (JSONException e) {
-                                            e.printStackTrace();
-                                        }
-                                    }
-                                }, new Response.ErrorListener() {
-                                    @Override
-                                    public void onErrorResponse(VolleyError error) {
-                                    }
-                                });
-                                queue1.add(request1);
-                            }else{
-                                Toast.makeText(getApplicationContext(), "you entered an Invalid Email", Toast.LENGTH_LONG).show();
+                        @Override
+                        public void onResponse(JSONObject response) {
+                            try {
+                                String res = response.getString("status");
+                                if (res.equals("success")) {
+                                    Intent login = new Intent(PasswordReset.this, Login.class);
+                                    PasswordReset.this.startActivity(login);
+                                } else if (res.equals("unauthorized")) {
+                                    Toast.makeText(getApplicationContext(), "Invalid credentials", Toast.LENGTH_LONG).show();
+                                } else {
+                                    Toast.makeText(getApplicationContext(), "An error has occurred, please try again", Toast.LENGTH_LONG).show();
+                                }
+                            } catch (JSONException e) {
+                                e.printStackTrace();
                             }
-                        } catch (JSONException e) {
-                            e.printStackTrace();
                         }
-                    }
-                }, new Response.ErrorListener() {
-
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-
-                    }
-                });
-                queue.add(request);
-
-
+                    }, new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            Toast.makeText(getApplicationContext(), "An error has occurred, please try again", Toast.LENGTH_LONG).show();
+                        }
+                    });
+                    queue.add(request);
+                }
             }
         });
-
 
         cancel.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -128,5 +92,20 @@ public class PasswordReset extends AppCompatActivity {
                 PasswordReset.this.startActivity(nav);
             }
         });
+    }
+
+    private boolean validateData() {
+        if(password.getText().toString().isEmpty() ||
+            newPassword.getText().toString().isEmpty() ||
+            email.getText().toString().isEmpty()) {
+            Toast.makeText(getApplicationContext(), "Please complete all fields", Toast.LENGTH_LONG).show();
+            return false;
+        }
+        if (!(newPassword.getText().toString().matches(PASSWORD_PATTERN))) {
+            Toast.makeText(getApplicationContext(), "Invalid Password, Must contain at least 1 digit, 1 lower case letter, 1 uppercase letter, 1 special character and" +
+                    " be more than 8 characters", Toast.LENGTH_LONG).show();
+            return false;
+        }
+        return true;
     }
 }
