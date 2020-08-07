6a60ad220385ba4ffef9cb0671b76667404a20e5
Omri <ogolan3589@gmail.com>
2020-08-07 12:51:31 -0400

    added MatchHistory activity

---
+++ app/src/main/java/com/prj666/recycling_vision/MatchHistory.java
@@ -0,0 +1,14 @@
+package com.prj666.recycling_vision;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MatchHistory extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_match_history_recyclerview);
+    }
+}
