2ec5d5842a6f60fd06696d47e2c2ead72e58cd3e
Omri <ogolan3589@gmail.com>
2020-08-07 13:08:46 -0400

    added onClick listener for back arrow

--- app/src/main/java/com/prj666/recycling_vision/MatchHistory.java
+++ app/src/main/java/com/prj666/recycling_vision/MatchHistory.java
@@ -2,7 +2,10 @@ package com.prj666.recycling_vision;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MatchHistory extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class MatchHistory extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_match_history_recyclerview);
+
+        ImageView backArrow = findViewById(R.id.left_arrow);
+        backArrow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), Navigation.class).setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
+            }
+        });
     }
 }
