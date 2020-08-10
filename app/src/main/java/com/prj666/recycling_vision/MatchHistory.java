2d28e0b60a3d6dfc081c709467f553b7467cd736
Anojan Sothilingam <30425339+asothilingam@users.noreply.github.com>
2020-08-09 23:36:10 -0400

    Update MatchHistory.java

--- app/src/main/java/com/prj666/recycling_vision/MatchHistory.java
+++ app/src/main/java/com/prj666/recycling_vision/MatchHistory.java
@@ -14,6 +14,43 @@ public class MatchHistory extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_match_history_recyclerview);
 
+         ArrayList<MatchHistoryItem> matchHistoryItem = new ArrayList<MatchHistoryItem>();
+        RequestQueue queue = Volley.newRequestQueue(this);
+
+        String url = "https://recycling-vision.herokuapp.com/matchhistoryitem";
+        JSONArray json = new JSONArray();
+        JSONObject jo = new JSONObject();
+        try {
+            jo.put("userID", "12");
+            json.put(jo);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        JsonArrayRequest request = new JsonArrayRequest(
+                Request.Method.POST, url, json, new Response.Listener<JSONArray>() {
+
+            @Override
+            public void onResponse(JSONArray response) {
+                /*try {
+                    for (int i = 0; i < response.length(); i++) {
+                        matchHistoryItem.add(new MatchHistoryItem(response.get(i).,
+                                response.getInt("objectID"),  response.getInt("userID"),
+                                response.getString("foundRecyclingInstruction"),
+                                (Date) response.get("matchDateTime")));
+                    }
+
+                    } catch (JSONException ex) {
+                    ex.printStackTrace();
+                }*/
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                System.out.println(error.toString());
+            }
+        });
+        queue.add(request);
         ImageView backArrow = findViewById(R.id.left_arrow);
         backArrow.setOnClickListener(new View.OnClickListener() {
             @Override
