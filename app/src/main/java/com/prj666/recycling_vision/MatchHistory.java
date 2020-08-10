0286a35f13a531f07e9acd286be16fd3de76b1b9
Omri <ogolan3589@gmail.com>
2020-08-10 13:35:19 -0400

    Update MatchHistory.java

--- app/src/main/java/com/prj666/recycling_vision/MatchHistory.java
+++ app/src/main/java/com/prj666/recycling_vision/MatchHistory.java
@@ -18,6 +18,7 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.Volley;
+import com.prj666.recycling_vision.user.Login;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -42,49 +43,54 @@ public class MatchHistory extends AppCompatActivity {
         RecyclerView rv = findViewById(R.id.match_history_recyclerview_container);
 
         String url = "https://recycling-vision.herokuapp.com/matchhistoryitem";
-        JSONArray json = new JSONArray();
-        JSONObject jo = new JSONObject();
+        JSONArray jsonArray = new JSONArray();
+        JSONObject userIDJson = new JSONObject();
         try {
-            jo.put("userID", "12");
-            json.put(jo);
+            userIDJson.put("userID", Login.getUserId());
+            jsonArray.put(userIDJson);
         } catch (JSONException e) {
             e.printStackTrace();
         }
 
         JsonArrayRequest request = new JsonArrayRequest(
-                Request.Method.POST, url, json, new Response.Listener<JSONArray>() {
+                Request.Method.POST, url, jsonArray, new Response.Listener<JSONArray>() {
             @Override
             public void onResponse(JSONArray response) {
-                try {
-                    for (int i = 0; i < response.length(); i++)
-                    {
-                        JSONObject item = response.getJSONObject(i);
-                        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd", Locale.ENGLISH);
-                        Date matchDate = dateFormat.parse(item.getString("matchDateTime").substring(0, item.getString("matchDateTime").length()-14));
-                        byte[] array = Base64.decode( item.getString("objectImage"), Base64.DEFAULT);
-                        Bitmap bm = BitmapFactory.decodeByteArray(array, 0, array.length);
+                if (response.length() > 0) {
+                    try {
+                        for (int i = 0; i < response.length(); i++) {
+                            JSONObject item = response.getJSONObject(i);
+                            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd", Locale.ENGLISH);
+                            Date matchDate = dateFormat.parse(item.getString("matchDateTime").substring(0, item.getString("matchDateTime").length() - 14));
+                            byte[] array = Base64.decode(item.getString("objectImage"), Base64.DEFAULT);
+                            Bitmap bm = BitmapFactory.decodeByteArray(array, 0, array.length);
 
-                        matchHistoryItem.add(new MatchHistoryItem(
-                                item.getInt("historyItemID"),
-                                item.getInt("objectID"),
-                                item.getInt("userID"),
-                                item.getString("foundRecyclingInstruction"),
-                                matchDate
-                        ));
-                        matchHistoryItem.get(i).setObjectImageBitmap(bm);
-                        matchHistoryItem.get(i).setObjectName(item.getString("objectName"));
-                        matchHistoryItem.get(i).setProbabilityMatch(item.getDouble("probabilitymatch"));
+                            matchHistoryItem.add(new MatchHistoryItem(
+                                    item.getInt("historyItemID"),
+                                    item.getInt("objectID"),
+                                    item.getInt("userID"),
+                                    item.getString("foundRecyclingInstruction"),
+                                    matchDate
+                            ));
+                            matchHistoryItem.get(i).setObjectImageBitmap(bm);
+                            matchHistoryItem.get(i).setObjectName(item.getString("objectName"));
+                            matchHistoryItem.get(i).setProbabilityMatch(item.getDouble("probabilitymatch"));
 
-                        runOnUiThread(()->{
-                            MatchHistoryRecyclerAdapter matchHistoryRecyclerAdapter = new MatchHistoryRecyclerAdapter(getApplication(), matchHistoryItem);
-                            rv.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
-                            rv.setAdapter(matchHistoryRecyclerAdapter);
-                        });
+                            runOnUiThread(() -> {
+                                MatchHistoryRecyclerAdapter matchHistoryRecyclerAdapter = new MatchHistoryRecyclerAdapter(getApplication(), matchHistoryItem);
+                                rv.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+                                rv.setAdapter(matchHistoryRecyclerAdapter);
+                            });
 
-                    }
+                        }
 
                     } catch (JSONException | ParseException ex) {
-                    ex.printStackTrace();
+                        ex.printStackTrace();
+                    }
+                }
+                else
+                {
+                    
                 }
             }
         }, new Response.ErrorListener() {
